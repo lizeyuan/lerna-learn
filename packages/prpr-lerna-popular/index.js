9e7b32bf1b59f5a4a040c0ed2f0d60774445b1ed
hailuo <hailuo@pinduoduo.com>
2020-12-24 10:45:33 +0800

    123

--- packages/prpr-lerna-popular/index.js
+++ packages/prpr-lerna-popular/index.js
@@ -4,5 +4,5 @@ const axios = require('axios');
 const getPopularImg = () => axios.get(API)
 
 module.exports = getPopularImg;
-
+console.log(32)
 getPopularImg().then((res) => console.log(res.data.length));
